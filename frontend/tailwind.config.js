ae1628a9a7a7dfb534e50c8e71d2d6ed2908d608
Jiri Tomasek <jiri.tomasek@suse.com>
2023-11-07 17:09:20 +0100

    Initial Tailwind css setup, data source tweaks (#10)
    * Prettier config update
    
    * Define SUSE theme colors
    
    * Tailwind configuration
    
    * Update the data source file
    
    * Update .gitignore
    
    Co-authored-by: Eduardo Mínguez <e.minguez@gmail.com>
    
    ---------
    
    Co-authored-by: Eduardo Mínguez <e.minguez@gmail.com>

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,15 +1,21 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  content: [
-    `./src/pages/**/*.{js,jsx,ts,tsx}`,
-    `./src/components/**/*.{js,jsx,ts,tsx}`,
-  ],
+  content: [`./src/pages/**/*.{js,jsx,ts,tsx}`, `./src/components/**/*.{js,jsx,ts,tsx}`],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        pineGreen: '#0C322C',
+        jungleGreen: '#30BA78',
+        midnightBlue: '#192072',
+        waterholeBlue: '#2453FF',
+        mint: '#90EBCD',
+        persimmon: '#FE7C3F',
+        fog: '#EFEFEF',
       },
     },
   },
