626d35c997d9dbf50d5f812df09cef2031462ca6
Jiri Tomasek <jiri.tomasek@suse.com>
2023-11-10 14:30:11 +0100

    Modal instead of dropdown

---
+++ frontend/src/components/SubversionsModal.js
@@ -0,0 +1,63 @@
+import React, { Fragment } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+
+export default function SubversionsModal({ versions, close, isOpen }) {
+  return (
+    <Transition.Root show={isOpen} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={close}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="bg-gray-500 fixed inset-0 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="bg-white relative transform overflow-hidden rounded-lg text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <div className="p-6">
+                  {versions.map((version) => (
+                    <div key={version.version}>
+                      <div className="flex flex-row items-center justify-between">
+                        <a
+                          href={version['github-release-link']}
+                          target="_blank"
+                          rel="noreferrer"
+                          className="text-gray-700 block px-4 py-2 text-sm hover:underline"
+                        >
+                          {version.version}
+                        </a>
+                        {version.prerelease && (
+                          <span className="bg-gray-50 text-gray-600 ring-gray-500/10 me-2 inline-flex items-center rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset">
+                            pre-release
+                          </span>
+                        )}
+                        <span className="text-gray-700 block px-4 py-2 text-sm">
+                          {version.released}
+                        </span>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+}
--- frontend/src/components/VersionsDropdown.js
+++ frontend/src/components/VersionsDropdown.js
@@ -1,7 +1,8 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { Menu, Transition } from '@headlessui/react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import SubversionsModal from './SubversionsModal';
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
@@ -9,8 +10,9 @@ function classNames(...classes) {
 
 export default function VersionsDropdown({ versions = [] }) {
   const [firstVersion, ...olderVersions] = versions;
+  const [modalOpen, setModalOpen] = useState(false);
   return (
-    <Menu as="div" className="relative inline-block w-full text-left">
+    <div className="relative inline-block w-full text-left">
       <div className="flex">
         <a
           className="bg-white text-gray-900 ring-gray-300 hover:bg-gray-50 w-full shrink-0 justify-center gap-x-1.5 rounded-md rounded-r-none px-3 py-2 text-left text-sm font-semibold shadow-sm ring-1 ring-inset"
@@ -20,20 +22,27 @@ export default function VersionsDropdown({ versions = [] }) {
         >
           {firstVersion.version}
         </a>
-        <Menu.Button
+        <button
           disabled={!olderVersions.length}
           className="bg-white text-gray-900 ring-gray-300 hover:bg-gray-50 -ml-px inline-flex w-full justify-center gap-x-1.5 rounded-md rounded-l-none px-3 py-2 pl-2 text-sm font-semibold shadow-sm ring-1 ring-inset"
           title="Older versions"
+          onClick={() => setModalOpen(true)}
         >
           <FontAwesomeIcon
             className="text-gray-400 -mr-1 h-5 w-5"
             aria-hidden="true"
             icon={faChevronDown}
           />
-        </Menu.Button>
+        </button>
       </div>
 
-      <Transition
+      <SubversionsModal
+        versions={olderVersions}
+        isOpen={modalOpen}
+        close={() => setModalOpen(false)}
+      />
+
+      {/* <Transition
         as={Fragment}
         enter="transition ease-out duration-100"
         enterFrom="transform opacity-0 scale-95"
@@ -42,28 +51,36 @@ export default function VersionsDropdown({ versions = [] }) {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="bg-white ring-black absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md shadow-lg ring-1 ring-opacity-5 focus:outline-none">
+        <Menu.Items className="bg-white ring-black absolute right-0 z-10 mt-2 w-full origin-top-right rounded-md shadow-lg ring-1 ring-opacity-5 focus:outline-none">
           <div className="py-1">
             {olderVersions.map((version) => (
               <Menu.Item key={version.version}>
                 {({ active }) => (
-                  <a
-                    href={version['github-release-link']}
-                    target="_blank"
-                    rel="noreferrer"
-                    className={classNames(
-                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                      'block px-4 py-2 text-sm'
+                  <div className="flex flex-row items-center justify-between">
+                    <a
+                      href={version['github-release-link']}
+                      target="_blank"
+                      rel="noreferrer"
+                      className={classNames(
+                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                        'block px-4 py-2 text-sm'
+                      )}
+                    >
+                      {version.version}
+                    </a>
+                    {version.prerelease && (
+                      <span className="bg-gray-50 text-gray-600 ring-gray-500/10 me-2 inline-flex items-center rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset">
+                        pre-release
+                      </span>
                     )}
-                  >
-                    {version.version}
-                  </a>
+                    {version.released}
+                  </div>
                 )}
               </Menu.Item>
             ))}
           </div>
         </Menu.Items>
-      </Transition>
-    </Menu>
+      </Transition> */}
+    </div>
   );
 }
