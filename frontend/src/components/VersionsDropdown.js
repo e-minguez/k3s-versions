8f7fa735fc820e1aba26b4d0bc919b1046e61f89
Jiri Tomasek <jiri.tomasek@suse.com>
2023-12-12 11:47:53 +0100

    Fix version version in versions list (#41)

--- frontend/src/components/VersionsDropdown.js
+++ frontend/src/components/VersionsDropdown.js
@@ -3,33 +3,37 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import SubversionsModal from './SubversionsModal';
 
-export default function VersionsDropdown({ versions = [] }) {
+export default function VersionsDropdown({ version }) {
   const [modalOpen, setModalOpen] = useState(false);
   return (
     <div className="relative inline-block w-full text-left">
       <div className="flex">
         <a
-          className="bg-white text-gray-900 ring-gray-300 hover:bg-gray-50 w-full shrink-0 justify-center gap-x-1.5 rounded-md rounded-r-none px-3 py-2 text-left text-sm font-semibold shadow-sm ring-1 ring-inset"
+          className="w-full shrink-0 justify-center gap-x-1.5 rounded-md rounded-r-none bg-white px-3 py-2 text-left text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
           target="_blank"
           rel="noreferrer"
-          href={versions[0]['github-release-link']}
+          href={version['github-release-link']}
         >
-          {versions[0].version}
+          {version.version}
         </a>
         <button
-          className="bg-white text-gray-900 ring-gray-300 hover:bg-gray-50 -ml-px inline-flex w-full justify-center gap-x-1.5 rounded-md rounded-l-none px-3 py-2 pl-2 text-sm font-semibold shadow-sm ring-1 ring-inset"
+          className="-ml-px inline-flex w-full justify-center gap-x-1.5 rounded-md rounded-l-none bg-white px-3 py-2 pl-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
           title="Older versions"
           onClick={() => setModalOpen(true)}
         >
           <FontAwesomeIcon
-            className="text-gray-400 -mr-1 h-5 w-5"
+            className="-mr-1 h-5 w-5 text-gray-400"
             aria-hidden="true"
             icon={faChevronDown}
           />
         </button>
       </div>
 
-      <SubversionsModal versions={versions} isOpen={modalOpen} close={() => setModalOpen(false)} />
+      <SubversionsModal
+        versions={version['all-versions']}
+        isOpen={modalOpen}
+        close={() => setModalOpen(false)}
+      />
     </div>
   );
 }
